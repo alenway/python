b87aea969fd824848c0f1adc749ba304dad9da05
Narendra Samtani <alenway97@gmail.com>
2025-01-12 22:00:01 +0530

    operators in python

---
+++ 01 Basic Syntax/05 operators/operators.py
@@ -0,0 +1,22 @@
+# adding
+a = 9
+b = 3
+
+# exponential
+print(5**3)
+
+# floar division
+print(5 // 2)
+
+# assignment and adding
+x = 4
+x += 3
+print(x)
+
+# Terneary operator
+x = 10
+result = "Even" if x % 2 == 0 else "Odd"
+print(result)  # Output: Even
+
+# concatenation
+print("hello" + "world")

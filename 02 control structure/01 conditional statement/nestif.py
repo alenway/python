c903ad0f4d91b2320e3e422f842826cd2a89922d
Narendra Samtani <alenway97@gmail.com>
2025-01-18 14:43:38 +0530

    solving question using conditional statement

--- 02 control structure/01 conditional statement/nestif.py
+++ 02 control structure/01 conditional statement/nestif.py
@@ -1,10 +1,19 @@
-x = 15
-if x > 10:
-    if x < 20:
-        print("x is smaller than 20")
-    else:
-        print("x is greater than 20")
-elif x == 15:
-    print("x is 15")
+x = int(input("Enter a number:"))
+# if x > 10:
+#     if x < 20:
+#         print("x is smaller than 20")
+#     else:
+#         print("x is greater than 20")
+# elif x == 15:
+#     print("x is 15")
+# else:
+#     print("x is out of this")
+#
+# '''print("comments")
+
+if x >0:
+    print("Number is positive")
+elif x < 0:
+    print("the number is negative")
 else:
-    print("x is out of this")
+    print("enter a valid number")

c959e815b54e8c5eb3a69b7b5d7dd4f05484e0f5
Narendra Samtani <alenway97@gmail.com>
2025-01-23 10:40:58 +0530

    jfjf

--- 03 data structures/lists.py
+++ 03 data structures/lists.py
@@ -12,11 +12,39 @@
 # list_ds = [5,6,7,'list-data',5.555]
 # print(list_ds)
 
-list_ds = [5,6,7]
-print(list_ds)
-list_ds.append([123,45])
-print(list_ds)
-list_ds.extend([567, 'example-extend'])
-print(list_ds)
-list_ds.insert([1,2])
-print(list_ds)
+# list_ds = [5,6,7]
+# print(list_ds)
+# list_ds.append([123,45])
+# print(list_ds)
+# list_ds.extend([567, 'example-extend'])
+# print(list_ds)
+# list_ds.insert([1,2])
+# print(list_ds)
+
+# list_dsp = [5,6,7]
+# print(list_dsp)
+# list_dsp.append([123, 45])
+# print(list_dsp)
+# list_dsp.extend([567,'example-of--insert-func'])
+
+#Accessing elements in the list
+# list_dsp = [5,6,7,'access-the-elements',4.55,20,50]
+# #for <variable name> in <sequence>:
+# for value in list_dsp:
+#     print(value,sep = ",",end = " ")
+# print(list_dsp)
+# print(list_dsp[2])
+# print(list_dsp[0:3])
+# print(list_dsp[::-1])
+
+
+
+#Questions
+#1create a list of 20 elements and display it in reverse order.
+twenty = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+twenty.reverse()
+print(twenty)
+#2.create 2 list with 5 five elements each conbine both the list and display it.
+#3consider the list with following [20,30,40,20,50] find and replace the first occurance of 20 with 10.
+#4create a list which consist of course of mca sem 2. at index 5 substitute the course name with preplacement traning
+#5create a list of 10 numbers and display the sum of the numbers.
